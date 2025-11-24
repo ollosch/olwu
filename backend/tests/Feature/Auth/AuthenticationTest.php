e030242d731ecd3cc28b44f8ab319411927e240f
birusch <15702230+birusch@users.noreply.github.com>
2025-11-24 17:02:43 +0100

    test(api/auth) User login and logout

---
+++ backend/tests/Feature/Auth/AuthenticationTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+use function Pest\Laravel\postJson;
+use function Pest\Laravel\withoutExceptionHandling;
+use function Pest\Laravel\withToken;
+
+beforeEach(function () {
+    $this->credentials = [
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ];
+
+    $this->user = User::factory()->create([
+        'email' => $this->credentials['email'],
+        'password' => Hash::make($this->credentials['password']),
+    ]);
+});
+
+it('can issue a personal API token', function () {
+    withoutExceptionHandling();
+    postJson(route('login'), $this->credentials)
+        ->assertStatus(201)
+        ->assertJsonStructure(['token']);
+
+    $this->assertDatabaseHas('personal_access_tokens', [
+        'tokenable_id' => $this->user->id,
+        'tokenable_type' => User::class,
+    ]);
+});
+
+test('users can not authenticate with invalid password', function () {
+    postJson(route('login'), [
+        'email' => $this->credentials['email'],
+        'password' => 'wrong-password',
+    ])
+        ->assertStatus(422)
+        ->assertJsonValidationErrors(['email']);
+
+    $this->assertGuest();
+});
+
+test('users can logout', function () {
+    $response = postJson(route('login'), $this->credentials);
+    $token = $response->json('token');
+
+    $response = withToken($token)->postJson('api/logout');
+
+    $response->assertNoContent();
+
+    $this->assertDatabaseMissing('personal_access_tokens', [
+        'tokenable_id' => $this->user->id,
+        'tokenable_type' => User::class,
+    ]);
+});
