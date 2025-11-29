722cad8cb79d6deac000daf6c414565221ea23d4
birusch <15702230+birusch@users.noreply.github.com>
2025-11-29 16:04:18 +0100

    chore(ui/auth) Show user name and logout, for testing purposes

--- frontend/src/views/AppLayout.vue
+++ frontend/src/views/AppLayout.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+
+const auth = useAuthStore()
+
+const onLogout = async () => {
+  await auth.logout()
+}
+</script>
 
 <template>
   <div class="min-h-screen">
+    <nav class="flex justify-between">
+      <div>Menu</div>
+      <div>
+        <button @click="onLogout">Logout</button>
+      </div>
+    </nav>
     <slot></slot>
   </div>
 </template>
--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import AppLayout from './AppLayout.vue'
+
+import { useAuthStore } from '@/stores/auth'
+const { user } = useAuthStore()
 </script>
 
 <template>
   <AppLayout>
     <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
+    <p>Welcome to your dashboard {{ user?.name }}!</p>
   </AppLayout>
 </template>
