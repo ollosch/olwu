3c2a61f24688663eccf057754364700b2fb9e5a9
birusch <15702230+birusch@users.noreply.github.com>
2025-12-02 11:05:47 +0100

    docs(ui) Remove misleading doc block

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -8,22 +8,6 @@ const router = createRouter({
   routes: [...authRoutes, ...appRoutes],
 })
 
-/**
- * Navigation Guard
- *
- * @remarks
- * This guard checks the user's authentication status before each route change.
- * It ensures that:
- * - Routes requiring authentication are only accessible to authenticated users.
- * - Routes requiring guest access are only accessible to unauthenticated users.
- * - If the user's auth token is present but their session is not validated,
- *   it attempts to validate the session by calling `checkAuth`.
- *
- * If a user tries to access a protected route without being authenticated,
- * they are redirected to the login page with an optional `redirect` query
- * parameter indicating their original destination. Conversely, authenticated
- * users trying to access guest-only routes are redirected to the dashboard.
- */
 router.beforeEach(async (to) => {
   const auth = useAuthStore()
   const requiresAuth = to.meta.requiresAuth
