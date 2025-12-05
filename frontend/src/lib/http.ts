71201de7256f98d688bdeb91dacb105a6b11e91a
birusch <15702230+birusch@users.noreply.github.com>
2025-12-05 15:12:21 +0100

    refac(ui/lib) Move actual error handling to Vue's global handler

--- frontend/src/lib/http.ts
+++ frontend/src/lib/http.ts
@@ -6,8 +6,6 @@ import type {
   InternalAxiosRequestConfig,
 } from 'axios'
 
-import { handleHttpErrors } from './errorHandler'
-
 export interface ResponseErrors {
   [key: string]: string
 }
@@ -57,13 +55,49 @@ axiosInstance.interceptors.response.use(
     if (handled) return Promise.reject(handled)
 
     if (!error.response) {
-      // toast('Network error') // optional
       return Promise.reject({ type: 'network', message: error.message } as ApiError)
     }
 
     return Promise.reject({ type: 'unknown', original: data } as ApiError)
   },
 )
+
+export function handleHttpErrors(
+  status: number | undefined,
+  data: { message?: string; errors?: LaravelErrors },
+): ApiError | false {
+  const message = data?.message
+
+  switch (status) {
+    case 401:
+      return { type: 'auth', message }
+    case 403:
+      return { type: 'forbidden', message }
+    case 422: {
+      if (data.errors) {
+        return { type: 'validation', errors: mapLaravelErrors(data.errors) }
+      }
+      break
+    }
+  }
+
+  return false
+}
+
+function mapLaravelErrors(laravelErrors: LaravelErrors) {
+  const errors: ResponseErrors = {}
+
+  if (laravelErrors) {
+    Object.keys(laravelErrors).forEach((key) => {
+      const errorValue = laravelErrors[key]
+
+      if (errorValue) {
+        errors[key] = Array.isArray(errorValue) ? errorValue[0] || '' : errorValue
+      }
+    })
+  }
+  return errors
+}
 
 export async function request<T>(config: AxiosRequestConfig): Promise<T> {
   const res = await axiosInstance.request<T>(config)
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,8 +3,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import type { ApiError } from './lib/http'
+import { toast } from 'vue-sonner'
 
 import '@/styles.css'
+import { useAuthStore } from './stores/auth'
 
 const app = createApp(App)
 
@@ -13,8 +16,34 @@ app.use(router)
 
 app.mount('#app')
 
-app.config.errorHandler = (err, instance, info) => {
+app.config.errorHandler = async (err, instance, info) => {
+  const error = err as ApiError
+
   if (import.meta.env.VITE_APP_DEBUG === 'true') {
     console.error('Global error handler:', err, instance, info)
+  }
+
+  if (!error) {
+    console.error('Error is empty:', err, instance, info)
+  }
+
+  switch (error.type) {
+    case 'auth':
+      try {
+        await useAuthStore().logout()
+      } finally {
+        toast.error('You are not logged in. Please log in to continue.')
+        router.push({ name: 'login' })
+      }
+      break
+    case 'forbidden':
+      toast.error('You do not have permission to access this resource.')
+      break
+    case 'validation':
+      // Laravel 422 errors have already been handled in useApi
+      break
+    case 'network':
+      toast.error('Network error. Please check your internet connection.')
+      break
   }
 }
