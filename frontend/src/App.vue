f24d0ba37155253407a26bb8564958c8747d6c11
birusch <15702230+birusch@users.noreply.github.com>
2025-12-05 15:10:32 +0100

    install(ui/shad) Add Toast (Sonner)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import 'vue-sonner/style.css'
+import { Toaster } from '@/components/ui/sonner'
+</script>
 
 <template>
-  <router-view class="dark bg-black text-green-400" />
+  <div>
+    <router-view class="dark bg-black text-green-400" />
+    <Toaster />
+  </div>
 </template>
